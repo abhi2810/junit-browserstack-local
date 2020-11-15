13240bb6f71a2d09bfeb8b062debb955a7a5a128
Abhi Singh <abhidashing.singh@gmail.com>
2020-11-16 00:14:01 +0530

    add local test

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -1,21 +1,29 @@
 package com.browserstack;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SingleTest extends BrowserStackJUnitTest {
 
-  @Test
-  public void test() throws Exception {
-    driver.get("https://www.google.com/ncr");
-    WebElement element = driver.findElement(By.name("q"));
-    element.sendKeys("BrowserStack");
-    element.submit();
-    Thread.sleep(5000);
-
-    assertEquals("BrowserStack - Google Search", driver.getTitle());
-  }
+	@Test
+	public void test() throws Exception {
+		ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+			}
+		};
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://localhost:3000/");
+		wait.until(pageLoadCondition);
+		System.out.println(driver.getTitle());
+		assertEquals(true, true);
+	}
 }
